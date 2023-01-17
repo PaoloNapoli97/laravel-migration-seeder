61b0eb1e23986be06ae2713c506f2c97b582d3fa
PaoloNapoli97 <paolonapoli@live.it>
2023-01-17 17:47:07 +0100

    Aggiunto database

--- database/migrations/2023_01_17_153811_create_train_table.php
+++ database/migrations/2023_01_17_153811_create_train_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('train', function (Blueprint $table) {
+        Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string('company', 100);
             $table->string('departure_station', 100);
