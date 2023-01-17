914ac58671ec00d879fc91771938746e9c669fff
PaoloNapoli97 <paolonapoli@live.it>
2023-01-17 17:16:30 +0100

    first commit

---
+++ database/migrations/2023_01_17_153811_create_train_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('train', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 100);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->date('departure_time');
+            $table->date('arrival_time');
+            $table->string('train_id', 20);
+            $table->unsignedTinyInteger('train_carriage');
+            $table->string('in_time', 10);
+            $table->string('cancelled', 5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('train');
+    }
+};
